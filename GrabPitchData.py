6d959651f44e72d3c9659efdd5bdd07c64c88807
Adrian Veto <aveto@umich.edu>
2026-07-06 18:12:10 -0400

    finished pitch velocities

--- GrabPitchData.py
+++ GrabPitchData.py
@@ -6,6 +6,7 @@ import pyarrow.dataset as pads
 import pyarrow.csv as pacsv
 import sportypy
 import sqlite3
+from scipy.integrate import quad
 from data.SMT_Data_Starter import readDataSubset
 
 ## Goals:
@@ -14,11 +15,11 @@ from data.SMT_Data_Starter import readDataSubset
 ## 2. (later file) Use start and end time to tag ball positions throughout the pitch
 ## 3. Classify pitches by pitch type and find outcome (HR / barrel / not HR or barrel)
 
-# Pulling ball events data, filtering to only PHD away team for smaller testing sample size
+# Pulling ball events data, filtering to only VAS away team for smaller testing sample size
 ball_events_subset = readDataSubset('ball-events',data_path="/Users/adrianveto/Downloads/Michigan/FirstPitchFastballFiesta/data")
-ball_events = ball_events_subset.to_table(filter = (pads.field('away_team') == "PHD")).to_pandas()
+ball_events = ball_events_subset.to_table(filter = (pads.field('home_team') == "VAS")).to_pandas()
 # print("Number of events: " + str(ball_events.size))
-# ~160k events for PHD away team
+# ~160k events for VAS away team
 
 # Filtering for events where ball is pitched
 Pitches = ball_events[ball_events["ball_eventcode"] == 1]
@@ -39,7 +40,7 @@ Pitches = Pitches[["game_string", "play_per_game", "pitch_release_time", "pitch_
 
 # now incorporating the lineup data to get pitcher ID and first pitch data
 lineups_subset = readDataSubset('lineups',data_path="/Users/adrianveto/Downloads/Michigan/FirstPitchFastballFiesta/data")
-lineups = lineups_subset.to_table(filter = (pads.field('away_team') == "PHD")).to_pandas()
+lineups = lineups_subset.to_table(filter = (pads.field('home_team') == "VAS")).to_pandas()
 
 Pitches = Pitches.merge(lineups[["game_string", "play_per_game", "pitcher", "batter"]],
               on=["game_string", "play_per_game"], how="left")
@@ -73,7 +74,7 @@ Pitches = Pitches[["game_string", "play_per_game", "pitch_release_time", "pitch_
 # calculate velocity from first and final point and delta_y
 
 ball_positions_subset = readDataSubset('ball-positions',data_path="/Users/adrianveto/Downloads/Michigan/FirstPitchFastballFiesta/data")
-ball_positions = ball_positions_subset.to_table(filter = (pads.field('away_team') == "PHD")).to_pandas()
+ball_positions = ball_positions_subset.to_table(filter = (pads.field('home_team') == "VAS")).to_pandas()
 
 # create a temporary df to get ALL ball positions with game string and PPG
 temp_df = pd.merge(ball_positions[["game_string", "play_per_game", "timestamp", "ball_position_x", "ball_position_y", "ball_position_z"]], 
@@ -101,7 +102,31 @@ PitchPositions = PitchPositions.merge(second_pos, on=["game_string", "play_per_g
 PitchPositions = PitchPositions.merge(final_pos, on=["game_string", "play_per_game"], how="left", suffixes=("_left", "_right"))
 PitchPositions = PitchPositions.drop(columns=["pitch_release_time_y", "pitch_end_time_y", "pitch_release_time_left",
                              "pitch_end_time_left", "pitch_release_time_right", "pitch_end_time_right", "index", "index_y", "index_x"])
-PitchPositions = PitchPositions.rename(columns={"pitch_release_time_x":"pitch_release_time",
-                                                "pitch_end_time_x":"pitch_end_time"})
-print(PitchPositions.columns)
+PitchPositions = PitchPositions.drop(columns=["pitch_release_time", "pitch_end_time"])
+# print(PitchPositions.head())
+
+# Now that we have the positions, we want to project through the first two points to find an expected endpoint at the final x and z
+PitchPositions["delta_y"] = PitchPositions["first_ball_position_y"] - PitchPositions["final_ball_position_y"] # in feet
+PitchPositions["time"] = PitchPositions["final_timestamp"] - PitchPositions["first_timestamp"] # in ms
+PitchPositions["velocity"] = round((PitchPositions["delta_y"] / PitchPositions["time"]) * 681.818, 2)  ## to convert ft/ms -> mph
+
+# PitchPositions["partial_time"] = PitchPositions["second_timestamp"] - PitchPositions["first_timestamp"]
+# PitchPositions["velo_x"] = (PitchPositions["first_ball_position_x"] - PitchPositions["second_ball_position_x"]) / PitchPositions["partial_time"]
+# PitchPositions["velo_z"] = (PitchPositions["first_ball_position_z"] - PitchPositions["second_ball_position_z"]) / PitchPositions["partial_time"]
+
+# PitchPositions["projected_x"] = PitchPositions["first_ball_position_x"] + (PitchPositions["velo_x"] * PitchPositions["time"])
+# # MLB projected z uses gravity as well. Faster version of ours skips the integral -> more break on everything
+# # PitchPositions["projected_z"] = PitchPositions["first_ball_position_z"] + (PitchPositions["velo_z"] * PitchPositions["time"])
+# PitchPositions["projected_z"] = PitchPositions["first_ball_position_z"] + (PitchPositions["velo_z"] * PitchPositions["time"]) - (4.905 * ((PitchPositions["time"] / 1000) ** 2))
+
+# PitchPositions["horizontal_break"] = PitchPositions["final_ball_position_x"] - PitchPositions["projected_x"]
+# PitchPositions["vertical_break"] = PitchPositions["final_ball_position_z"] - PitchPositions["projected_z"]
+
+# PitchPositions = PitchPositions.drop(columns=["delta_y", "time", "partial_time", "velo_x", "velo_z", "projected_x", "projected_z"])
+PitchPositions = PitchPositions.drop(columns=["delta_y", "time"])
+PitchPositions = PitchPositions.drop(columns=["first_timestamp", "first_ball_position_x", "first_ball_position_y", "first_ball_position_z",
+                                              "second_timestamp", "second_ball_position_x", "second_ball_position_y", "second_ball_position_z",
+                                              "final_timestamp", "final_ball_position_x", "final_ball_position_y", "final_ball_position_z"])
 print(PitchPositions.head())
+
+PitchPositions.to_csv("VAS_pitches.csv", index=False)
